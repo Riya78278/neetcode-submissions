5dac73efbdb245e79cef800a5323407816029f0d
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-06-27 13:23:46 +0530

    Add: minimum-cost-for-tickets - submission-1

---
+++ Data Structures & Algorithms/minimum-cost-for-tickets/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    int dp[366];
+    int solve (vector<int>& days,vector<int>& costs,int n, int i){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        //for 1days
+        int cost_1=costs[0]+solve(days,costs,n,i+1);
+
+        //for 7 days
+        int j=i;
+        int maxdays=days[j]+7;
+
+        while(j<n && days[j]<maxdays){
+            j++;
+        }
+        int cost_7=costs[1]+solve(days,costs,n,j);
+
+        // for 30 days
+        int k=i;
+        int maxidays=days[k]+30;
+
+        while(k<n && days[k]<maxidays){
+            k++;
+        }
+        int cost_30=costs[2]+solve(days,costs,n,k);
+
+        return dp[i]=min({cost_1,cost_7,cost_30});
+
+
+    }
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        for(int i=0;i<366;i++){
+            dp[i]=-1;
+        }
+        int n=days.size();
+        return solve(days,costs,n,0);
+    }
+};
