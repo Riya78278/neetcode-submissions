4c5d7b2fe82f374a6e9e6aed0e53aed8d6a5ca46
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-07-01 16:42:12 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int solve(string &s,int i,int j){
+        if(i>=j){
+            return true;
+        }
+
+        if(s[i]==s[j]){
+            return solve(s,i+1,j-1);
+        }
+        return false;
+    }    
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+
+        int maxlen=INT_MIN;
+        int start=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)==true){
+                    if(j-i+1 > maxlen){
+                        maxlen= j-i+1;
+                        start=i;
+                    }
+                }
+            }
+        }
+        return s.substr(start,maxlen);
+    }
+};
